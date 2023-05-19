ceabd194521872a7ffee6f3f4f14563880caaf18
rajaha <hardik.raja@bc.edu>
2023-05-19 17:17:48 +0530

    made as above with simple API

--- src/main/java/com/graphql/learn/controllers/BookController.java
+++ src/main/java/com/graphql/learn/controllers/BookController.java
@@ -3,90 +3,31 @@ package com.graphql.learn.controllers;
 import com.graphql.learn.entity.Book;
 import com.graphql.learn.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @MutationMapping("createBook")
-    public Book create(@Argument BookInput book){
-        Book b = new Book();
-        b.setTitle(book.getTitle());
-        b.setDesc(book.getDesc());
-        b.setPrice(book.getPrice());
-        b.setAuthor(book.getAuthor());
-        b.setPages(book.getPages());
-        return this.bookService.create(b);
+    @PostMapping("/book/create")
+    public Book create(@RequestBody Book book){
+        return this.bookService.create(book);
     }
 
-    @QueryMapping("allBooks")
+    @GetMapping("/books")
     public List<Book> getAll(){
         return this.bookService.getAll();
     }
 
-    @QueryMapping("getBook")
-    public Book get(@Argument Integer bookId){
+    @GetMapping("/book/id/{bookId}")
+    public Book get(@PathVariable Integer bookId){
         return this.bookService.get(bookId);
     }
 
 
 
-}
-
-class BookInput{
-    private String title;
-    private String desc;
-    private String author;
-    private double price;
-    private int pages;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
 }
