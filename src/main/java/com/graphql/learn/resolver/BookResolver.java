8cafcff680c1df165d2dc6db5147e86e2b40a977
rajaha <hardik.raja@bc.edu>
2023-05-22 12:59:25 +0530

    with netflix

--- src/main/java/com/graphql/learn/resolver/BookResolver.java
+++ src/main/java/com/graphql/learn/resolver/BookResolver.java
@@ -2,6 +2,8 @@ package com.graphql.learn.resolver;
 
 import com.graphql.learn.entity.Book;
 import com.graphql.learn.service.BookService;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -9,13 +11,14 @@ import org.springframework.stereotype.Controller;
 import javax.jws.Oneway;
 import java.util.List;
 
+@DgsComponent
 @Controller
 public class BookResolver {
 
     @Autowired
     private BookService bookService;
 
-    @QueryMapping
+    @DgsQuery
     public List<Book> books(){
         return bookService.getAll();
     }
