6ff0ab4d57e9b24578c0b8169fdb27a6c765c165
rajaha <hardik.raja@bc.edu>
2023-05-22 16:25:24 +0530

    with spqr

--- src/main/java/com/graphql/learn/resolver/BookResolver.java
+++ src/main/java/com/graphql/learn/resolver/BookResolver.java
@@ -2,20 +2,20 @@ package com.graphql.learn.resolver;
 
 import com.graphql.learn.entity.Book;
 import com.graphql.learn.service.BookService;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-import javax.jws.Oneway;
 import java.util.List;
 
-@Controller
-public class BookResolver {
+@Service
+public class BookResolver  {
 
     @Autowired
     private BookService bookService;
 
-    @QueryMapping
+
+    @GraphQLQuery
     public List<Book> books(){
         return bookService.getAll();
     }
